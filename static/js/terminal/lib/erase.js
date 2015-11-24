29ec604905d1014ee0825c64f718dd801e11ed61
nuintun <nuintun@gmail.com>
2015-11-24 19:00:34 +0800

    update files

---
+++ static/js/terminal/lib/erase.js
@@ -0,0 +1,88 @@
+'use strict';
+
+module.exports = function (Terminal){
+  Terminal.prototype.eraseRight = function (x, y){
+    var line = this.lines[this.ybase + y],
+      ch = [this.curAttr, ' ']; // xterm
+
+    for (; x < this.cols; x++) {
+      line[x] = ch;
+    }
+
+    this.updateRange(y);
+  };
+
+  Terminal.prototype.eraseLeft = function (x, y){
+    var line = this.lines[this.ybase + y],
+      ch = [this.curAttr, ' ']; // xterm
+
+    x++;
+    while (x--) line[x] = ch;
+
+    this.updateRange(y);
+  };
+
+  Terminal.prototype.eraseLine = function (y){
+    this.eraseRight(0, y);
+  };
+
+  // CSI Ps J Erase in Display (ED).
+  // Ps = 0 -> Erase Below (default).
+  // Ps = 1 -> Erase Above.
+  // Ps = 2 -> Erase All.
+  // Ps = 3 -> Erase Saved Lines (xterm).
+  // CSI ? Ps J
+  // Erase in Display (DECSED).
+  // Ps = 0 -> Selective Erase Below (default).
+  // Ps = 1 -> Selective Erase Above.
+  // Ps = 2 -> Selective Erase All.
+  Terminal.prototype.eraseInDisplay = function (params){
+    var j;
+    switch (params[0]) {
+      case 0:
+        this.eraseRight(this.x, this.y);
+        j = this.y + 1;
+        for (; j < this.rows; j++) {
+          this.eraseLine(j);
+        }
+        break;
+      case 1:
+        this.eraseLeft(this.x, this.y);
+        j = this.y;
+        while (j--) {
+          this.eraseLine(j);
+        }
+        break;
+      case 2:
+        j = this.rows;
+        while (j--) this.eraseLine(j);
+        break;
+      case 3:
+        ; // no saved lines
+        break;
+    }
+  };
+
+  // CSI Ps K Erase in Line (EL).
+  // Ps = 0 -> Erase to Right (default).
+  // Ps = 1 -> Erase to Left.
+  // Ps = 2 -> Erase All.
+  // CSI ? Ps K
+  // Erase in Line (DECSEL).
+  // Ps = 0 -> Selective Erase to Right (default).
+  // Ps = 1 -> Selective Erase to Left.
+  // Ps = 2 -> Selective Erase All.
+  Terminal.prototype.eraseInLine = function (params){
+    switch (params[0]) {
+      case 0:
+        this.eraseRight(this.x, this.y);
+        break;
+      case 1:
+        this.eraseLeft(this.x, this.y);
+        break;
+      case 2:
+        this.eraseLine(this.y);
+        break;
+    }
+  };
+};
---
+++ static/js/terminal/lib/open.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// if bold is broken, we can't
+// use it in the terminal.
+function isBoldBroken(){
+  var el = document.createElement('span');
+  el.innerHTML = 'hello world';
+  document.body.appendChild(el);
+  var w1 = el.scrollWidth;
+  el.style.fontWeight = 'bold';
+  var w2 = el.scrollWidth;
+  document.body.removeChild(el);
+  return w1 !== w2;
+}
+
+module.exports = function (Terminal){
+  /**
+   * Open Terminal
+   */
+
+  Terminal.prototype.open = function (){
+    var self = this,
+      i = 0,
+      div;
+
+    this.element = document.createElement('div');
+    this.element.className = 'terminal';
+    this.children = [];
+
+    for (; i < this.rows; i++) {
+      div = document.createElement('div');
+      this.element.appendChild(div);
+      this.children.push(div);
+    }
+
+    this.refresh(0, this.rows - 1);
+
+    // XXX - hack, move this somewhere else.
+    if (Terminal.brokenBold === null) {
+      Terminal.brokenBold = isBoldBroken();
+    }
+
+    // sync default bg/fg colors
+    this.element.style.backgroundColor = Terminal.defaultColors.bg;
+    this.element.style.color = Terminal.defaultColors.fg;
+  };
+};
