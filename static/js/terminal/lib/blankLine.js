1ffe4052e64d3f9db498e7a00b6ae2c93c714d25
nuintun <nuintun@gmail.com>
2015-11-25 12:54:54 +0800

    update files

--- static/js/terminal/lib/blankLine.js
+++ static/js/terminal/lib/blankLine.js
@@ -7,7 +7,7 @@
 module.exports = function (Terminal){
   Terminal.prototype.blankLine = function (cur){
     var attr = cur ? this.curAttr : this.defAttr;
-    var ch = [attr, ' '];
+    var ch = [attr, ''];
     var line = [];
     var i = 0;
 
