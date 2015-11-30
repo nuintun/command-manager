a6a3a75e04853a143ce0ddb98f22a52bc1cea6b8
nuintun <nuintun@gmail.com>
2015-11-30 15:52:44 +0800

    update files

--- static/js/terminal/xterm.js
+++ static/js/terminal/xterm.js
@@ -584,15 +584,15 @@ function _width_wcwidth(ucs, opts){
   // if we arrive here, ucs is not a combining or C0/C1 control character
   return 1 +
     (ucs >= 0x1100 &&
-    (ucs <= 0x115f ||                       // Hangul Jamo init. consonants
+    (ucs <= 0x115f || // Hangul Jamo init. consonants
     ucs == 0x2329 || ucs == 0x232a ||
     (ucs >= 0x2e80 && ucs <= 0xa4cf &&
-    ucs != 0x303f) ||                     // CJK ... Yi
-    (ucs >= 0xac00 && ucs <= 0xd7a3) ||    // Hangul Syllables
-    (ucs >= 0xf900 && ucs <= 0xfaff) ||    // CJK Compatibility Ideographs
-    (ucs >= 0xfe10 && ucs <= 0xfe19) ||    // Vertical forms
-    (ucs >= 0xfe30 && ucs <= 0xfe6f) ||    // CJK Compatibility Forms
-    (ucs >= 0xff00 && ucs <= 0xff60) ||    // Fullwidth Forms
+    ucs != 0x303f) ||  // CJK ... Yi
+    (ucs >= 0xac00 && ucs <= 0xd7a3) || // Hangul Syllables
+    (ucs >= 0xf900 && ucs <= 0xfaff) || // CJK Compatibility Ideographs
+    (ucs >= 0xfe10 && ucs <= 0xfe19) || // Vertical forms
+    (ucs >= 0xfe30 && ucs <= 0xfe6f) || // CJK Compatibility Forms
+    (ucs >= 0xff00 && ucs <= 0xff60) || // Fullwidth Forms
     (ucs >= 0xffe0 && ucs <= 0xffe6) ||
     (ucs >= 0x20000 && ucs <= 0x2fffd) ||
     (ucs >= 0x30000 && ucs <= 0x3fffd)));
@@ -1033,7 +1033,7 @@ AnsiTerminal.prototype.toString = function (type){
   if (type === 'html') {
     for (i = 0; i < this.screen.buffer.length; ++i) {
       for (j = 0; j < this.screen.buffer[i].cells.length; ++j) {
-        console.log(this.screen.buffer[i].cells[j].getAttributes());
+        console.log(this.screen.buffer[i].cells[j]);
       }
     }
   } else {
