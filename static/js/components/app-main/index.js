6ad95b2156d40dc9f840f96b6366a06929e55c62
nuintun <nuintun@gmail.com>
2015-12-03 10:41:40 +0800

    update files

--- static/js/components/app-main/index.js
+++ static/js/components/app-main/index.js
@@ -209,13 +209,12 @@ module.exports = Vue.component('app-main', {
 
       switch (type) {
         case 'data':
-          data = data.toString();
           break;
         case 'error':
-          data = '\u001b[31m发生错误: \u001b[0m' + data.toString();
+          data = '\u001b[31m发生错误: \u001b[0m' + data;
           break;
         case 'close':
-          data = '\u001b[32m命令执行完成\u001b[0m';
+          data = '\u001b[32m命令执行完成\u001b[0m\r\n';
           break;
       }
 
